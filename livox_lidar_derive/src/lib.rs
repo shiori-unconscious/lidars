722e6aa5ee67cd4d481846b9cc750a592a6f9dd0
ShioriAya <shioriaya@foxmail.com>
2024-03-12 17:32:00 +0800

    derive marco

--- livox_lidar_derive/src/lib.rs
+++ livox_lidar_derive/src/lib.rs
@@ -8,6 +8,16 @@ use syn::{self, Data, DeriveInput};
 pub fn check_status_derive(input: TokenStream) -> TokenStream {
     let ast: DeriveInput = syn::parse(input).unwrap();
     let name = &ast.ident;
+    let Data::Struct(ds) = ast.data else {
+        panic!("Trait CheckStatus derive must be use on struct");
+    };
+    if ds
+        .fields
+        .iter()
+        .any(|f| f.ident.as_ref().is_some_and(|name| name == "ret_code"))
+    {
+        panic!("Trait CheckStatus needs struct field 'ret_code'");
+    }
     let gen = quote! {
         impl CheckStatus for #name {
             fn check_status(&self) -> bool {
@@ -23,15 +33,15 @@ pub fn len_derive(input: TokenStream) -> TokenStream {
     let ast: DeriveInput = syn::parse(input).unwrap();
     let id = &ast.ident;
     let Data::Struct(ds) = ast.data else {
-        panic!("Len Derive Must be Use on struct");
+        panic!("Trait Len derive must be use on struct");
     };
+    let len = ds.fields.iter().fold(0u16, |accumulate,f|{
+        accumulate + std::mem::size_of::<{ f.ty }>() as u16
+    });
     let gen = quote! {
         impl Len for #id {
             fn len() -> u16 {
-                for f in ds.fields {
-
-                }
-                std::mem::size_of<>()
+                #len
             }
         }
     };
